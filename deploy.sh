dc423628fdd4ea6a1eba83e9898b6911c5fc8327
Drew Steen <andrew.decker.steen@gmail.com>
2020-09-08 16:45:52 -0400

    add deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+# This should be run from the source code directory, not from public
+
+# If a command fails then the deploy stops
+set -e
+
+printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
+
+# Build the project.
+hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
+
+# Go To Public folder
+cd public
+
+# Add changes to git.
+git add .
+
+# echo -n "Please enter the commit message:"
+# read msg
+# echo "Your message was $msg"
+
+# Commit changes.
+msg="rebuilding site $(date)"
+if [ -n "$*" ]; then
+	msg="$*"
+fi
+git commit -m "$msg"
+
+# Push source and build repos.
+git push origin master 
+
+# Go back up to the original directory
+cd ..
