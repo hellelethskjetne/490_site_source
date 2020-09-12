4e4a6abe52542d79265c109f0facb594726840c1
Drew Steen <andrew.decker.steen@gmail.com>
2020-09-12 16:23:49 -0400

    add hard-coded hyperlink

--- deploy.sh
+++ deploy.sh
@@ -7,6 +7,15 @@ set -e
 
 printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
 
+# Commit changes to source code
+msg="rebuilding site $(date)"
+if [ -n "$*" ]; then
+        msg="$*"
+fi
+
+
+
+
 # Build the project.
 hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
 
